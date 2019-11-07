79890737552f6bc2bc9414b5a056306c1ed2087d
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2019-11-07 23:25:14 +0000

    memfh

--- memfh.c
+++ memfh.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MEMFH_PAGELEN 4096
+
+#define MEMFH_MAX_ITEMS(keylen) ((MEMFH_PAGELEN - 8) / (keylen + sizeof(char *)))
 
 typedef struct memfh_data_s {
     struct memfh_data_s *next;
     char *record;
 } memfh_data_t;    
+
+typedef struct memfh_idx_s {
+    char tp;
+    char filler[3];
+    int  n;    
+    char buf[MEMFH_PAGELEN-8];
+} memfh_idx_t;    
 
 typedef struct memfh_hdr_s {
     char *filename;    
@@ -14,6 +26,7 @@ typedef struct memfh_hdr_s {
     int   keylen;
     memfh_data_t *head;
     memfh_data_t *tail;
+    memfh_idx_t *idx;
 } memfh_hdr_t;    
 
 memfh_hdr_t *memfh_open(char *filename, int reclen, int keylen) {
@@ -21,11 +34,15 @@ memfh_hdr_t *memfh_open(char *filename, int reclen, int keylen) {
     memfh_hdr_t *hdr;
 
     hdr = malloc(sizeof(memfh_hdr_t));
+    hdr->filename = strdup(filename);
     hdr->count = 0;
     hdr->reclen = reclen;
     hdr->keylen = keylen;
     hdr->head = NULL;
     hdr->tail = NULL;
+    hdr->idx = malloc(sizeof(memfh_idx_t));
+    hdr->idx->tp = 0;
+    hdr->idx->n = 0;
 
     return hdr;        
 }        
@@ -41,6 +58,10 @@ void memfh_close(memfh_hdr_t *hdr) {
         free(data);    
         data = next;
     }
+
+    free(hdr->idx);
+    free(hdr->filename);
+    free(hdr);
 }        
 
 void memfh_list(memfh_hdr_t *hdr) {
@@ -55,6 +76,57 @@ void memfh_list(memfh_hdr_t *hdr) {
     }
 
 }        
+
+void memfh_idx_list(memfh_hdr_t *hdr, memfh_idx_t *idx) {
+
+    char *ptr, key[257], *record;
+
+    fprintf(stderr, "idx n=%d\n", idx->n);    
+    ptr = idx->buf;
+    for (int i=0; i<idx->n; i++) {
+        memcpy(key, ptr, hdr->keylen);
+        key[hdr->keylen] = 0;    
+        memcpy(&record, ptr+hdr->keylen, sizeof(char *));
+        fprintf(stderr, "%d [%s] [%s]\n", i, key, record);
+        ptr += (hdr->keylen + sizeof(char *));
+    }        
+}
+
+void memfh_idx_write(memfh_hdr_t *hdr, char *key, char *record) {
+
+    memfh_idx_t *idx;
+    char *ptr;
+
+    idx = hdr->idx;
+
+    if (idx->tp == 0) {
+        // folha
+        if (idx->n < MEMFH_MAX_ITEMS(hdr->keylen)) {
+            // procura a posicao de insercao    
+            ptr = idx->buf;   
+            for (int i=0; i<idx->n; i++) {
+                if (memcmp(key, ptr, hdr->keylen) < 0) {
+                    fprintf(stderr, "TODO abrir espaco\n");
+                    exit(-1);
+                }        
+                ptr += (hdr->keylen + sizeof(char *));
+            }
+            memcpy(ptr, key, hdr->keylen);
+            memcpy(ptr+hdr->keylen, &record, sizeof(char *));
+            idx->n++;
+            memfh_idx_list(hdr, idx);
+
+        } else {
+            fprintf(stderr, "TODO pagina cheia\n");
+            exit(-1);
+
+        }        
+    } else {
+        fprintf(stderr, "TODO no interno\n");
+        exit(-1);
+    }    
+
+}
 
 void memfh_write(memfh_hdr_t *hdr, char *record) {
 
@@ -73,6 +145,9 @@ void memfh_write(memfh_hdr_t *hdr, char *record) {
         hdr->tail->next = data;
         hdr->tail = data;    
     }    
+
+    //TODO montar chave
+    memfh_idx_write(hdr, data->record, data->record);
 
     hdr->count++;
 }
