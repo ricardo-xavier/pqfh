29ff31b480010a7d659ba3970751dca54a17b364
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2023-07-10 22:23:13 -0300

    wip:parser

--- json/json.c
+++ json/json.c
@@ -4,7 +4,15 @@
 #include "json.h"
 
 void load(char *filename, char **buf);
-void parse(jobject_t obj, int i);
+int parse(jobject_t *obj, char *json, int i);
+
+#define ST_START_OBJ   'O'
+#define ST_START_NAME  'N'
+#define ST_NAME        'n'
+#define ST_COLON       ':'
+#define ST_START_VALUE 'V'
+
+char st;
 
 int main(int argc, char *argv[]) {
     char *json;
@@ -15,12 +23,86 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     load(argv[1], &json);
-    printf("%s\n", json);
-    strcpy(root.name, "/");
-    parse(root, 0);
+    st = ST_START_OBJ;
+    memset(&root, 0, sizeof(jobject_t));
+    parse(&root, json, 0);
     free(json);
     return 0;
 }
 
-void parse(jobject_t obj, int i) {
+int parse(jobject_t *obj, char *json, int i) {
+    printf("parse %s\n", obj->name);    
+    jobject_t child;    
+    for (;;) {    
+        printf("i=%d %c st=%c\n", i, json[i], st);
+        switch (st) {
+            case ST_START_OBJ:
+                switch (json[i]) {
+                    case ' ':
+                    case '\n':
+                        break;    
+                    case '[':
+                        obj->isArray = true;    
+                        break;
+                    case '{':
+                        memset(&child, 0, sizeof(jobject_t));
+                        st = ST_START_NAME;
+                        i = parse(&child, json, ++i);
+                        obj->children = list2_append(obj->children, &child, sizeof(jobject_t));
+                        break;    
+                    default:
+                        fprintf(stderr, "PARSE ERR %d '%c' st=%c\n", i, json[i], st);
+                        exit(-1);
+                        break;    
+                }        
+                break;
+
+            case ST_START_NAME:
+                switch (json[i]) {
+                    case ' ':
+                    case '\n':
+                        break;    
+                    case '"':
+                        st = ST_NAME;
+                        break;
+                    default:
+                        fprintf(stderr, "PARSE ERR %d '%c' st=%c\n", i, json[i], st);
+                        exit(-1);
+                        break;    
+                }        
+                break;
+
+            case ST_NAME:
+                if (json[i] != '"') {
+                    int len = strlen(obj->name);
+                    obj->name[len++] = json[i];
+                    obj->name[len] = 0;
+                } else {
+                    st = ST_COLON;    
+                }        
+                break;        
+
+            case ST_COLON:
+                switch (json[i]) {
+                    case ' ':
+                    case '\n':
+                        break;    
+                    case ':':
+                        st = ST_START_VALUE;
+                        break;
+                    default:
+                        fprintf(stderr, "PARSE ERR %d '%c' st=%c\n", i, json[i], st);
+                        exit(-1);
+                        break;    
+                }
+                break;        
+
+            default:
+                fprintf(stderr, "PARSE ERR %d '%c' st=%c\n", i, json[i], st);
+                exit(-1);
+                break;    
+        }
+        i++;
+    }    
+    return i;
 }
--- json/json.h
+++ json/json.h
@@ -1,10 +1,16 @@
 #ifndef JSON_H
 #define JSON_H
 
+#include "../list2.h"
+
+#define bool unsigned char
+#define true 0
+
 typedef struct jobject_s {
     char name[257];
     char *value;
-    struct jobject_s *children;
+    bool isArray;
+    list2_t *children;
 } jobject_t;
 
 #endif
