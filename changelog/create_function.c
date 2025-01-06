0b67c11c1398e2dd2e8f681f24cc720c7e19d337
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2025-01-05 22:35:01 -0200

    changelog

---
+++ changelog/create_function.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+#include <libpq-fe.h>
+#include <time.h>
+
+void create_function() {
+    printf("TODO create_function\n");
+}
---
+++ changelog_trigger.cbl
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. changelogtrigger.
+
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+           call 'create_function'
+           stop run.
